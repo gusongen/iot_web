b7e5e190ee249845c569018fdb8fd0772ede12ce
gusongen <you@example.com>
2020-03-09 17:09:40 +0800

    0.3

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, re_path
 from main import views
 
 urlpatterns = [
-    path('', views.hello),
-    re_path("(?P<key>(.)*)/",views.test),
+    # path('', views.hello),
+    re_path('(.)*', views.hello),
+    # re_path("(?P<key>(.)*)/",views.test),
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -3,18 +3,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def hello(request):
-
-    info_dict =tuple(request.META.items())
-    for k, v in info_dict:
-        print("k::%s  v::%s" %(k,v))
-    return HttpResponse("hello")
-
-
-def test(request,key):
-    location=request.GET.get('location')
-    language=request.GET.get("language")
+def hello(request, *args):
     info_dict = tuple(request.META.items())
     for k, v in info_dict:
         print("k::%s  v::%s" % (k, v))
-    return JsonResponse({'mag':"ok",'status':200})
+    return JsonResponse({'mag': "ok", 'status': 200})
+
+
+def test(request, key):
+    location = request.GET.get('location')
+    language = request.GET.get("language")
+    info_dict = tuple(request.META.items())
+    for k, v in info_dict:
+        print("k::%s  v::%s" % (k, v))
+    return JsonResponse({'mag': "ok", 'status': 200})
